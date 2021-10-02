433cc62a89dce7bc53652b826297f20a26b09a40
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2021-10-02 16:15:05 +0000

    edits

--- reset.sh
+++ reset.sh
@@ -6,12 +6,19 @@ echo "This will reinitialize the database and delete the uploaded PDFs!"
 echo "Press CTRL+c to cancel or ENTER to continue."
 read
 
+
+
 DBPATH=./app/sql
 PDFPATH=./app/static/pdf
 PDFDB=$DBPATH/pdf.db
 USERSDB=$DBPATH/users.db
 
-echo -n "Creating database..."
+
+echo "Creating a backup..."
+BACKUP="tr-searcher-backup-$(date '+%F_%H-%M-%S').tar.gz"
+tar czvf $BACKUP $PDFPATH $PDFDB $USERSDB
+
+echo -n "Reinitializing database..."
 
 [[ -f $PDFDB ]] && rm $PDFDB
 [[ -f $USERSDB ]] && rm $USERSDB
