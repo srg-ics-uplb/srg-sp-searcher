c89caf720e08e511b37bbf8478d45e8007f22fb3
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2024-09-08 17:46:07 +0800

    edits compress pdf

--- compresspdf.sh
+++ compresspdf.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+TARGET=$1
+TARGET=${TARGET%%.pdf}
 
-gs -q -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dPDFSETTINGS=/screen -dEmbedAllFonts=true -dSubsetFonts=true -dColorImageDownsampleType=/Bicubic -dColorImageResolution=144 -dGrayImageDownsampleType=/Bicubic -dGrayImageResolution=144 -dMonoImageDownsampleType=/Bicubic -dMonoImageResolution=144 -sOutputFile=$1.compressed.pdf $1; 
+gs -q -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dPDFSETTINGS=/screen -dEmbedAllFonts=true -dSubsetFonts=true -dColorImageDownsampleType=/Bicubic -dColorImageResolution=144 -dGrayImageDownsampleType=/Bicubic -dGrayImageResolution=144 -dMonoImageDownsampleType=/Bicubic -dMonoImageResolution=144 -sOutputFile=$TARGET.compressed.pdf $1; 
